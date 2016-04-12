eb524b65bb17fd7674289708b29ae01120314e2f
Gautam Bisht <gbisht@lbl.gov>
2016-04-11 21:08:11 -0700

    updated travis-ci install scripts

--- .install-osx-deps.sh
+++ .install-osx-deps.sh
@@ -1,6 +1,7 @@
 # Install required software
 brew update
-brew install openmpi git
+brew install openmpi git 
+brew install netcdf --enable-fortran
 
 git clone https://bitbucket.org/petsc/petsc.git
 
