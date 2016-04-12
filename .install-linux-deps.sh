2da8c802252aa849feff162bccffb6fdf090f0ab
Gautam Bisht <gbisht@lbl.gov>
2016-04-12 07:43:49 -0700

    Updates travis-ci script to checkout out v3.5.4 of PETSc

--- .install-linux-deps.sh
+++ .install-linux-deps.sh
@@ -5,6 +5,8 @@ git clone https://bitbucket.org/petsc/petsc.git
 
 cd petsc
 
+git checkout v3.5.4
+
 export PETSC_DIR=$PWD
 export PETSC_ARCH=linux-gnu
 ./configure PETSC_ARCH=linux-gnu --with-mpi=1 --with-debug=$DEBUG --with-shared-libraries=1
--- .install-osx-deps.sh
+++ .install-osx-deps.sh
@@ -7,6 +7,8 @@ git clone https://bitbucket.org/petsc/petsc.git
 
 cd petsc
 
+git checkout v3.5.4
+
 export PETSC_DIR=$PWD
 export PETSC_ARCH=osx-gnu
 ./configure PETSC_ARCH=osx-gnu --with-mpi=1 --with-debug=$DEBUG --with-shared-libraries=1
