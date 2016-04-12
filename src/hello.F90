7dbf3605691e4d3f814085c3a49273f2f2d3a4ed
Gautam Bisht <gbisht@lbl.gov>
2016-04-11 18:09:08 -0700

    Updates the example code and cmake files to use PETSc.

--- src/hello.F90
+++ src/hello.F90
@@ -1,4 +1,38 @@
 program main
-  print *, 'Hello world! Why is everything in color?'
-end program
+#include "finclude/petscsys.h"
+#include "finclude/petscvec.h"
+#include "finclude/petscvec.h90"
+
+  Vec                 :: x
+  PetscInt, parameter :: n = 2
+  PetscInt            :: rank, size,i
+  PetscReal, pointer  :: x_p(:)
+  PetscErrorCode      :: ierr
+
+  call PetscInitialize(PETSC_NULL_CHARACTER,ierr)
+
+  PETSC_COMM_WORLD = MPI_COMM_WORLD
+
+  call MPI_Comm_rank(PETSC_COMM_WORLD, rank, ierr);CHKERRQ(ierr)
+  call MPI_Comm_size(PETSC_COMM_WORLD, size, ierr);CHKERRQ(ierr)
+
+  if (rank==0) write(*,*), 'Hello world! Why is everything in color?'
+
+  call VecCreate(PETSC_COMM_WORLD, x, ierr);CHKERRQ(ierr)
+  call VecSetSizes(x, n, PETSC_DECIDE, ierr);CHKERRQ(ierr)
+  call VecSetFromOptions(x);
+
+  call VecGetArrayF90(x, x_p, ierr);CHKERRQ(ierr)
+  do i = 1,n
+     x_p(i) = i + 10.d0*rank
+  enddo
+  call VecRestoreArrayF90(x, x_p, ierr);CHKERRQ(ierr)
+
+  call VecView(x, PETSC_VIEWER_STDOUT_WORLD, ierr);CHKERRQ(ierr)
+
+  call VecDestroy(x, ierr); CHKERRQ(ierr)
+
+  call PetscFinalize(ierr)
+
+end program main
 
