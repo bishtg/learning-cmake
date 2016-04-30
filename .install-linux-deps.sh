75258be5d5a0dad5f384282edad334adeabd9e32
bishtg <gbisht@gmail.com>
2016-04-30 13:00:33 -0700

    Updating Toolchains from PPA for linux bot

--- .install-linux-deps.sh
+++ .install-linux-deps.sh
@@ -1,3 +1,4 @@
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -qq
 sudo apt-get install -y cmake gcc-5 libopenmpi-dev openmpi-bin liblapack-dev gfortran-5 mercurial git netcdf-bin libnetcdf-dev 
 
