aebbd1a058fbbd0609eb9daf08b2df4b26920556
bishtg <gbisht@gmail.com>
2016-04-30 13:11:47 -0700

    output the path of gcc on linux bot

--- .install-linux-deps.sh
+++ .install-linux-deps.sh
@@ -2,6 +2,9 @@ sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -qq
 sudo apt-get install -y cmake gcc-5 libopenmpi-dev openmpi-bin liblapack-dev gfortran-5 mercurial git netcdf-bin libnetcdf-dev 
 
+which gcc-5
+which gcc
+
 git clone https://bitbucket.org/petsc/petsc.git
 
 cd petsc
