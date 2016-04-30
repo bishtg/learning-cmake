4fa5f11d19b818cba9200d44783770e52bb9a51d
bishtg <gbisht@gmail.com>
2016-04-30 12:43:19 -0700

    updates gcc to 5.x for linux travis ci builder

--- .install-linux-deps.sh
+++ .install-linux-deps.sh
@@ -1,5 +1,5 @@
 sudo apt-get update -qq
-sudo apt-get install -y cmake gcc libopenmpi-dev openmpi-bin liblapack-dev gfortran mercurial git netcdf-bin libnetcdf-dev 
+sudo apt-get install -y cmake gcc-5 libopenmpi-dev openmpi-bin liblapack-dev gfortran-5 mercurial git netcdf-bin libnetcdf-dev 
 
 git clone https://bitbucket.org/petsc/petsc.git
 
